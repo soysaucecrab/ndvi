9dffd46895db59cb57884755e193de5c07bf90ee
soysaucecrab <88924237+soysaucecrab@users.noreply.github.com>
2023-08-06 10:14:11 +0900

    Update serial.py

--- serial.py
+++ serial.py
@@ -1,12 +1,6 @@
 import serial
 import time
 
-# Set up the serial port
-ser = serial.Serial('/dev/ttyACM0', 115200)  # Change '/dev/ttyACM0' to the appropriate serial port
-
-status_value = [1, 2, 3, 4, 5, 6, 7, 8]
-status_len = 0
-
 def calculate_status_len(status_values):
     result = 0
     for value in status_values:
@@ -21,18 +15,52 @@ def calculate_status_len(status_values):
         result += 1  # for ','
     return result
 
-while True:
-    # Calculate sensor value string length
-    status_len = calculate_status_len(status_value)
+ser = serial.Serial('/dev/ttyACM0', 115200)  # Change '/dev/ttyACM0' to the appropriate serial port
 
-    # Send Message to main cansat
-    ser.write(bytes([0x76, 0x00, 0xA1, status_len]))
-    for value in status_value:
+StatusValue = [1, 2, 3, 4, 5, 6, 7, 8]
+StatusLen = 0
+
+while True:
+    # 1. first read the Status value then wait
+    for idx in range(8):
+        time.sleep(0.001)
+    
+    # 2. Calculate sensor value string length
+    StatusLen = calculate_status_len(StatusValue)
+    
+    # 3. Send Message to main cansat
+    ser.write(bytes([0x76, 0x00, 0xA1, StatusLen]))
+    for value in StatusValue:
         ser.write(str(value).encode())
         ser.write(b',')
-
+    
     ser.write(b'\n')
-    time.sleep(1)
+    time.sleep(0.001)
 
-# Note: This is a basic conversion of the Arduino code to Python for Raspberry Pi.
-#       You may need to adjust the serial port and other configurations to match your specific setup.
+def hyGs_RecvByte(b):
+    global sRecvMode
+    global cRecvCmd
+
+    # 56 00 .... 
+    if sRecvMode == 0x00:
+        sRecvMode = 0x01 if b == 0x56 else 0
+    elif sRecvMode == 0x01:
+        sRecvMode = 0x02 if b == 0x00 else 1 if b == 0x56 else 0
+    elif sRecvMode == 0x02:
+        sRecvMode = 0x10 if b == 0x48 else 1 if b == 0x56 else 0
+    elif sRecvMode == 0x10:
+        sRecvMode = sRecvMode + 1 if b == 0x00 else 1 if b == 0x56 else 0
+    elif sRecvMode == 0x11:
+        sRecvMode = sRecvMode + 1
+        cRecvCmd[0] = b
+    elif sRecvMode == 0x12:
+        sRecvMode = sRecvMode + 1
+        cRecvCmd[1] = b
+    elif sRecvMode == 0x13:
+        sRecvMode = 0
+        cRecvCmd[2] = b
+        if 64 <= cRecvCmd[0] < 64 + 8:  # when 8 bit data value
+            if (cRecvCmd[0] ^ cRecvCmd[1]) == cRecvCmd[2]:  # XOR
+                StatusValue[cRecvCmd[0] - 64] = cRecvCmd[1]
+    else:
+        sRecvMode = 1 if b == 0x56 else 0
