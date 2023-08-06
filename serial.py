08dce4dea50dcd1f1c2d960e1e475419b61c6f0d
soysaucecrab <88924237+soysaucecrab@users.noreply.github.com>
2023-08-06 09:48:49 +0900

    Update serial.py

--- serial.py
+++ serial.py
@@ -1,20 +1,38 @@
-import time          // 타임모듈 불러오기
-import serial        //pyserial 모듈 불러오기
+import serial
+import time
 
+# Set up the serial port
+ser = serial.Serial('/dev/ttyACM0', 115200)  # Change '/dev/ttyACM0' to the appropriate serial port
 
-ser = serial.Serial(                 // serial 객체 생성
-        port='/dev/ttyAMA1',         // 시리얼통신에 사용할 포트
-        baudrate=115200,                // 통신속도 지정
-        parity=serial.PARITY_NONE,       // 패리티 비트 설정방식
-        stopbits=serial.STOPBITS_ONE,     // 스톱비트 지정
-        bytesize=serial.EIGHTBITS,        // 데이터 비트수 지정
-        timeout=1                        // 타임아웃 설정
-        )
+status_value = [1, 2, 3, 4, 5, 6, 7, 8]
+status_len = 0
 
+def calculate_status_len(status_values):
+    result = 0
+    for value in status_values:
+        if value < 10:
+            result += 1
+        elif value < 100:
+            result += 2
+        elif value < 1000:
+            result += 3
+        elif value < 10000:
+            result += 4
+        result += 1  # for ','
+    return result
 
-while True:                         // 반복해서 숫자값을 입력받음, 이때 숫자가 아닌 값을 입력하면 다시 입력을 요구함
-    degree = input('what degree do you want for servo?\n')
-    if degree.isdigit():
-        ser.write(degree.encode())
-    else:
-        print("you have to pass a number") 
+while True:
+    # Calculate sensor value string length
+    status_len = calculate_status_len(status_value)
+
+    # Send Message to main cansat
+    ser.write(bytes([0x76, 0x00, 0xA1, status_len]))
+    for value in status_value:
+        ser.write(str(value).encode())
+        ser.write(b',')
+
+    ser.write(b'\n')
+    time.sleep(1)
+
+# Note: This is a basic conversion of the Arduino code to Python for Raspberry Pi.
+#       You may need to adjust the serial port and other configurations to match your specific setup.
